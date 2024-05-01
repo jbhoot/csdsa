076b3f960bf98bb0d424bd0324bd87874d05d707
Jayesh Bhoot <jysh@jysh.net>
2024-05-01 14:09:32 +0530

    Print result in intersection

--- intersection.ml
+++ intersection.ml
@@ -8,3 +8,14 @@ let rec intersection a b =
 match a with
 | [] -> []
 | hd :: tl -> if present_in hd b then hd :: intersection tl b else intersection tl b
+
+let rec make_res_print res =
+match res with
+| [] -> ""
+| hd :: [] -> Printf.sprintf "%d" hd
+| hd :: tl -> Printf.sprintf "%d; " hd ^ make_res_print tl
+
+let print_res res =
+Printf.printf "[%s]" (make_res_print res)
+
+let () = print_res (intersection [3; 5; 4; 2] [4; 5; 3; 6])
